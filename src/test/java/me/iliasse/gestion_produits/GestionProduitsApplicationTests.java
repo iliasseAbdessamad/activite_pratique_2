aba3bc9be942bae5e2e496a2980ec4a53695687e
iliasseAbdessamad <samwillh7@gmail.com>
2025-06-16 18:15:41 +0100

    Setup Project - Add project dependencies : Spring Data JPA - Spring Web - Spring Security - Spring Validation - Thymleaf - h2database - mysql - lombok.

---
+++ src/test/java/me/iliasse/gestion_produits/GestionProduitsApplicationTests.java
@@ -0,0 +1,13 @@
+package me.iliasse.gestion_produits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionProduitsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
