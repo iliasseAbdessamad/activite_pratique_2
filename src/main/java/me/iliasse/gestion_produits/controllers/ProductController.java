38207d0a784579515b2c6e5c2e47969d417cd49d
iliasseAbdessamad <samwillh7@gmail.com>
2025-06-17 13:53:19 +0100

    Add show() action in ProductController, ProductDetailsDTO, and product show view

--- src/main/java/me/iliasse/gestion_produits/controllers/ProductController.java
+++ src/main/java/me/iliasse/gestion_produits/controllers/ProductController.java
@@ -1,18 +1,26 @@
 package me.iliasse.gestion_produits.controllers;
 
+import me.iliasse.gestion_produits.dto.product.ProductDetailsDTO;
 import me.iliasse.gestion_produits.dto.product.ProductListingDto;
+import me.iliasse.gestion_produits.entities.Product;
 import me.iliasse.gestion_produits.repository.ProductRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.swing.text.html.Option;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 @Controller
 public class ProductController {
+
+    private static final String CURRENCY = "MAD";
 
     private ProductRepository productRepository;
 
@@ -23,11 +31,26 @@ public class ProductController {
     @GetMapping("/products")
     public String index(Model model){
         Set<ProductListingDto> productListingDto = productRepository.findAll().stream().map(p -> {
-            return new ProductListingDto(p, "MAD", 120);
+            return new ProductListingDto(p, ProductController.CURRENCY, 120);
         }).collect(Collectors.toSet());
 
         model.addAttribute("products", productListingDto);
 
         return "views/product/index";
+    }
+
+    @GetMapping("/products/{id}")
+    public String show(@PathVariable Long id, Model model){
+        try{
+            Product product = this.productRepository.findById(id).get();
+            ProductDetailsDTO productDetailsDTO = new ProductDetailsDTO(product, ProductController.CURRENCY);
+
+            model.addAttribute("product", productDetailsDTO);
+
+            return "views/product/show";
+
+        }catch(NoSuchElementException ex){
+            return "redirect:/products";
+        }
     }
 }
---
+++ src/main/java/me/iliasse/gestion_produits/dto/product/ProductDetailsDTO.java
@@ -0,0 +1,21 @@
+package me.iliasse.gestion_produits.dto.product;
+
+import lombok.Getter;
+import me.iliasse.gestion_produits.entities.Product;
+
+@Getter
+public final class ProductDetailsDTO extends ProductDto {
+
+    private String formattedDescription;
+    private int quantity;
+
+    public ProductDetailsDTO(Product product, String currency) {
+        super(product, currency);
+        this.formattedDescription = this.formatDescription(product.getDescription());
+        this.quantity = product.getQuantity();
+    }
+
+    private String formatDescription(String description){
+        return description.replace("\n", "<br />");
+    }
+}
