3b7d8accc0c5774102c45a26a1ae6d55333ff053
iliasseAbdessamad <samwillh7@gmail.com>
2025-06-22 05:53:29 +0100

    Add README and imgs folder to the project

--- src/main/java/me/iliasse/gestion_produits/repository/ProductRepository.java
+++ src/main/java/me/iliasse/gestion_produits/repository/ProductRepository.java
@@ -7,13 +7,21 @@ import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-
 public interface ProductRepository extends JpaRepository<Product, Long> {
     List<Product> findByPublishedTrue();
 
-    @Query("select p from Product p where p.published = true and (Lower(p.name) like lower(concat('%', :query, '%')) or cast(p.price as string) like %:query%)")
+    @Query("""
+    select p from Product p 
+    where p.published = true 
+    and (Lower(p.name) like lower(concat('%', :query, '%')) 
+    or cast(p.price as string) like %:query%)
+    """)
     List<Product> searchByNameOrPriceButPublishedTrue(@Param("query") String query);
 
-    @Query("select p from Product p where Lower(p.name) like lower(concat('%', :query, '%')) or cast(p.price as string) like %:query%")
+    @Query("""
+    select p from Product p 
+    where Lower(p.name) like lower(concat('%', :query, '%')) 
+    or cast(p.price as string) like %:query%
+    """)
     List<Product> searchByNameOrPrice(@Param("query") String query);
 }
